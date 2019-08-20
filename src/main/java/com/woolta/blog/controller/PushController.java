32bc8419cbc5b7816f7243bb0923f30e3723c19c
hwangseong-in <gommpo111@gmail.com>
2019-08-20 17:05:16 +0900

    test web-push success

--- src/main/java/com/woolta/blog/controller/PushController.java
+++ src/main/java/com/woolta/blog/controller/PushController.java
@@ -3,11 +3,13 @@ package com.woolta.blog.controller;
 import lombok.RequiredArgsConstructor;
 import nl.martijndwars.webpush.Notification;
 import nl.martijndwars.webpush.PushService;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.jose4j.lang.JoseException;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.security.Security;
 import java.util.concurrent.ExecutionException;
 
 @CrossOrigin(origins = "*")
@@ -18,12 +20,19 @@ public class PushController {
 
 
     @GetMapping("/notification")
-    public void push(@RequestParam String key, @RequestParam String auth) throws GeneralSecurityException, InterruptedException, JoseException, ExecutionException, IOException {
+    public void push(@RequestParam String key, @RequestParam String auth, @RequestParam String endPoint) throws GeneralSecurityException, InterruptedException, JoseException, ExecutionException, IOException {
+
+
+        // Add BouncyCastle as an algorithm provider
+        if (Security.getProvider(BouncyCastleProvider.PROVIDER_NAME) == null) {
+            Security.addProvider(new BouncyCastleProvider());
+        }
 
         PushService pushService = new PushService();
 
-        pushService.setPublicKey("AAAA9TjSk74:APA91bGojZrGMIyzm-OXXgQHwGhqviRcANDBXspcv69dx7YN-63HM2r0z0mPxXzyyeDghUvjB3zkPGYW8Hwqsr9b8KBwwliOeghYmBLwP-bEtABXu4L_5AtrrKZmPv6PbNoWsIPSCVRv");
-        Notification notification = new Notification("/", key, auth, "");
+        pushService.setPublicKey("");
+        pushService.setPrivateKey("");
+        Notification notification = new Notification(endPoint, key, auth, "1212121");
         pushService.send(notification);
     }
 }
